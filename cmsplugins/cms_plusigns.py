31321f2807fcb114fb5450ad001e58922d7c194f
Max <noelpmax@gmail.com>
2024-10-12 19:23:07 +0200

    create Plugin for ContentContainer

--- cmsplugins/cms_plusigns.py
+++ cmsplugins/cms_plusigns.py
@@ -1,2 +1,16 @@
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
+from  .models import ContentContainer
+from django.utils.translation import gettext_lazy as _
+
+@plugin_pool.register_plugin
+class ContentContainerPlugin(CMSPluginBase):
+    model = ContentContainer
+    name = _('Content Container')
+    render_template = 'content/content-container.html'
+    cache = False
+    allow_children = True
+
+    def render(self, context, instance, placeholder):
+        context = super().render(context, instance, placeholder)
+        return context
